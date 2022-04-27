d95095cab4859430f63b0668d52d60a697097219
lenonrust <lenon.rust@gmail.com>
2022-04-27 15:28:14 -0300

    concluindo requisito 16

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './helpers/Routes';
 import './App.css';
+import SearchProvider from './context/SearchProvider';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <SearchProvider>
+        <Routes />
+      </SearchProvider>
     </BrowserRouter>
   );
 }
