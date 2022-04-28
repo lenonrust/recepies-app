1d37bb274fe9f63f42f5e97229992dbc39d67534
lenonrust <lenon.rust@gmail.com>
2022-04-28 19:12:02 -0300

    concluindo telas de explorar e explorar comidas-bebidas

--- src/context/SearchProvider.js
+++ src/context/SearchProvider.js
@@ -5,12 +5,23 @@ import searchContext from './searchContext';
 function SearchProvider({ children }) {
   const [foods, setFoods] = useState([]);
   const [drinks, setDrinks] = useState([]);
-
+  const [search, setSearch] = useState({
+    inputText: '',
+    type: 'ingredient',
+  });
+  const [clickButton, setClickButton] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const contexValue = {
     foods,
     setFoods,
     drinks,
     setDrinks,
+    search,
+    setSearch,
+    clickButton,
+    setClickButton,
+    isVisible,
+    setIsVisible,
   };
 
   return (
--- src/pages/ExploreFoodsByIngredients.js
+++ src/pages/ExploreFoodsByIngredients.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import searchContext from '../context/searchContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const TWELV = 12;
+
 function ExploreFoodsbyIngredients() {
+  const [ingredients, setIngredients] = useState();
+  const { setSearch, setClickButton, setIsVisible } = useContext(searchContext);
+  const history = useHistory();
+
+  const handleIngredients = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    const response = await fetch(url);
+    const { meals } = await response.json();
+    setIngredients(meals);
+  };
+
+  useEffect(() => {
+    handleIngredients();
+  }, []);
+  if (!ingredients) {
+    return '';
+  }
+
+  const handleSearchIngredient = (ingredient) => {
+    setSearch({ inputText: ingredient, type: 'ingredient' });
+    setIsVisible(true);
+    setClickButton(true);
+    history.push('/foods');
+  };
   return (
     <div>
       <Header title="Explore Ingredients" />
+      { ingredients.slice(0, TWELV)
+        .map((itr, index) => (
+          <button
+            key={ `food-ingredient${index}` }
+            type="button"
+            onClick={ () => handleSearchIngredient(itr.strIngredient) }
+          >
+            <div data-testid={ `${index}-ingredient-card` }>
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ `https://www.themealdb.com/images/ingredients/${itr.strIngredient}-Small.png` }
+                alt={ itr.strIngredient }
+              />
+              <p data-testid={ `${index}-card-name` }>{itr.strIngredient}</p>
+
+            </div>
+          </button>
+        )) }
       <Footer />
     </div>
   );
