4b9b4f4993fc2167397cbe87ec9a223615f4d3d3
Elthon <elthon.rv@gmail.com>
2022-05-07 13:52:38 -0300

    criada função renderWithRouterAndProvider

---
+++ src/tests/renderWithProviderAndRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import SearchProvider from '../context/SearchProvider';
+
+const renderWithRouterAndProvider = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(
+      <Router history={ history }>
+        <SearchProvider>
+          {component}
+        </SearchProvider>
+      </Router>,
+    ),
+    history,
+  });
+};
+export default renderWithRouterAndProvider;
