975fb06ab21a31bc62585f94be34ec6e803ee9fe
Elthon <elthon.rv@gmail.com>
2022-05-07 14:07:54 -0300

    refatora teste 12

--- src/tests/12.Profile.test.js
+++ src/tests/12.Profile.test.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
-import SearchProvider from '../context/SearchProvider';
 import Routes from '../helpers/Routes';
+import renderWithRouterAndProvider, { history } from './renderWithProviderAndRouter';
 
 const fetchMock = require('../../cypress/mocks/fetch');
 
 global.fetch = fetchMock;
-const history = createMemoryHistory();
 
 beforeEach(async () => {
   await act(async () => {
-    render(
-      <Router history={ history }>
-        <SearchProvider>
-          <Routes />
-        </SearchProvider>
-      </Router>,
-    );
+    renderWithRouterAndProvider(<Routes />);
   });
 });
 
-describe('12 - Testa se'
-  + ' ', () => {
+describe('12 - Testa se', () => {
   const { getComputedStyle } = window;
   window.getComputedStyle = (elt) => getComputedStyle(elt);
 
@@ -49,31 +39,31 @@ describe('12 - Testa se'
     expect(emailDisplay).toBeInTheDocument();
   });
 
-  it('12.2 - Se o botão Done Recipes da tela de Perfil funciona conforme '
-      + ' protótipo', async () => {
-    history.push('/profile/');
+  it('12.2 - Se o botão Done Recipes da tela de Perfil funciona conforme protótipo',
+    async () => {
+      history.push('/profile/');
 
-    const doneBtn = await screen.findByRole('button', { name: /done recipes/i });
-    userEvent.click(doneBtn);
+      const doneBtn = await screen.findByRole('button', { name: /done recipes/i });
+      userEvent.click(doneBtn);
 
-    expect(history.location.pathname).toBe('/done-recipes');
-  });
+      expect(history.location.pathname).toBe('/done-recipes');
+    });
 
-  it('12.3 - Se o botão Favorite Recipes da tela de Perfil funciona'
-      + ' conforme protótipo', async () => {
-    history.push('/profile/');
+  it('12.3 - Se o botão Favorite Recipes da tela de Perfil funciona conforme protótipo',
+    async () => {
+      history.push('/profile/');
 
-    const favBtn = await screen.findByRole('button', { name: /favorite recipes/i });
-    userEvent.click(favBtn);
+      const favBtn = await screen.findByRole('button', { name: /favorite recipes/i });
+      userEvent.click(favBtn);
 
-    expect(history.location.pathname).toBe('/favorite-recipes');
-  });
+      expect(history.location.pathname).toBe('/favorite-recipes');
+    });
 
-  it('12.4 - Se o botão Logout da tela de Perfil funciona'
-      + ' conforme protótipo', async () => {
-    history.push('/profile/');
-    const logoutBtn = await screen.findByRole('button', { name: /logout/i });
-    userEvent.click(logoutBtn);
-    expect(history.location.pathname).toBe('/');
-  });
+  it('12.4 - Se o botão Logout da tela de Perfil funciona conforme protótipo',
+    async () => {
+      history.push('/profile/');
+      const logoutBtn = await screen.findByRole('button', { name: /logout/i });
+      userEvent.click(logoutBtn);
+      expect(history.location.pathname).toBe('/');
+    });
 });
